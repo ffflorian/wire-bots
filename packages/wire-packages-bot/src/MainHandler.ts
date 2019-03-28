89691deebbfd6f766bde86fd689ab31e6b3f125f
Florian Keller <github@floriankeller.de>
2019-03-28 10:18:56 +0100

    Adjust to new API

--- packages/wire-packages-bot/src/MainHandler.ts
+++ packages/wire-packages-bot/src/MainHandler.ts
@@ -2,7 +2,7 @@ import * as logdown from 'logdown';
 
 import {Connection, ConnectionStatus} from '@wireapp/api-client/dist/commonjs/connection';
 import {MessageHandler} from '@wireapp/bot-api';
-import {PayloadBundleIncoming, PayloadBundleType, ReactionType} from '@wireapp/core/dist/conversation/';
+import {PayloadBundle, PayloadBundleType, ReactionType} from '@wireapp/core/dist/conversation/';
 import {TextContent} from '@wireapp/core/dist/conversation/content/';
 import {CommandService, CommandType, ParsedCommand} from './CommandService';
 import {SearchService} from './SearchService';
@@ -44,7 +44,7 @@ class MainHandler extends MessageHandler {
     }
   }
 
-  async handleEvent(payload: PayloadBundleIncoming) {
+  async handleEvent(payload: PayloadBundle) {
     switch (payload.type) {
       case PayloadBundleType.TEXT: {
         if (payload.conversation) {
